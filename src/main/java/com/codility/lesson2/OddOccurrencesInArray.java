815f2d221b1270cdad1ed5b20a9aaf39a2b48b87
Andriy Simonov <andriy.simonov@gmail.com>
2019-10-14 12:54:37 +0300

    Changed odd occurrences search to array copy implementation

--- src/main/java/com/codility/lesson2/OddOccurrencesInArray.java
+++ src/main/java/com/codility/lesson2/OddOccurrencesInArray.java
@@ -3,23 +3,24 @@ package com.codility.lesson2;
 public class OddOccurrencesInArray {
     public int solution(int[] A) {
         int[] array = A;
-        outer: for (int i = 0; i < array.length; ++ i) {
-            if (array[i] != 0) {
-                for (int j = i + 1; j < array.length; ++ j) {
-                    if (array[i] == array[j]) {
-                        array[i] = array[j] = 0;
-                        continue outer;
-                    }
-                }
-            }
-        }
-        int result = 0;
+        int max = 0;
+        
         for (int i = 0; i < array.length; ++ i) {
-            if (array[i] != 0) {
-                result = array[i];
+            max = array[i] > max ? array[i] : max; 
+        }
+        int[] map = new int[max + 1];
+        for (int i = 0; i < array.length; ++ i) {
+            map[array[i]] += 1;
+        }
+        
+        int result = 0;
+        for (int i = 0; i < map.length; ++ i) {
+            if (map[i] % 2 != 0) {
+                result = i;
                 break;
             }
         }
+        
         return result;
     }
 }
